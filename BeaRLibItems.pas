bbaf9b1302b238defcf6db42941891d0bf91d6d3
Sergiy Tkach <bees@meta.ua>
2017-09-13 14:47:14 +0300

    Updated BeaRLibItems.dll.

--- BeaRLibItems.pas
+++ BeaRLibItems.pas
@@ -51,9 +51,11 @@ function Items_Dungeon_GetMapItem(MapID, Index: Integer): Item; stdcall; externa
 function Items_Dungeon_SetMapItemXY(MapID, Index: Integer; AX, AY: Integer; AItem: Item): Integer; stdcall; external 'BeaRLibItems.dll';
 function Items_Dungeon_GetMapItemXY(MapID, Index: Integer; AX, AY: Integer): Item; stdcall; external 'BeaRLibItems.dll';
 
-procedure Items_Dungeon_AppendItem(AItem: Item); stdcall; external 'BeaRLibItems.dll';
+procedure Items_Dungeon_AppendItem(AItem: Item; InHead: Boolean = False); stdcall; external 'BeaRLibItems.dll';
+
 function Items_Dungeon_DeleteItem(Index: Integer; var AItem: Item): Integer; stdcall; external 'BeaRLibItems.dll';
-function Items_Dungeon_DeleteItemXY(MapID: Integer; Index, AX, AY: Integer; var AItem: Item): Integer; stdcall; external 'BeaRLibItems.dll';
+function Items_Dungeon_DeleteMapItem(MapID: Integer; Index: Integer; var AItem: Item): Integer; stdcall; external 'BeaRLibItems.dll';
+function Items_Dungeon_DeleteMapItemXY(MapID: Integer; Index, AX, AY: Integer; var AItem: Item): Integer; stdcall; external 'BeaRLibItems.dll';
 
 function Items_Dungeon_GetMapItemAmountXY(MapID, ItemID, AX, AY: Integer): Integer; stdcall; external 'BeaRLibItems.dll';
 
