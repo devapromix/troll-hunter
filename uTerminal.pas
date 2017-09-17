58e8dcbb1f53614e3d989f60aba2c3effe952e27
Sergiy Tkach <bees@meta.ua>
2017-09-17 23:44:55 +0300

    Bug fix.

--- uTerminal.pas
+++ uTerminal.pas
@@ -173,7 +173,7 @@ begin
   FChar.Width := terminal_state(TK_CELL_WIDTH);
   FChar.Height := terminal_state(TK_CELL_HEIGHT);
   terminal_set(Format('icon font: Fontello.ttf, size=%dx%d, codepage=437;',
-    [FChar.Width, FChar.Height]));
+    [FChar.Width + 5, FChar.Height + 5]));
 end;
 
 procedure TTerminal.Print(AX, AY: Integer; AText: string; Align: Byte);
