8bd39dddc0084c46f0f164cefd45d9d8dfaf832a
Sergiy Tkach <bees@meta.ua>
2017-11-09 12:18:48 +0200

    Quests.

--- uAffixes.pas
+++ uAffixes.pas
@@ -1,4 +1,4 @@
-unit uAffixes;
+﻿unit uAffixes;
 
 interface
 
--- uQuest.pas
+++ uQuest.pas
@@ -1,6 +1,15 @@
-unit uQuest;
+﻿unit uQuest;
 
 interface
+
+{ TODO -cУбить существо : Убить уникального босса на локации (от первой до предпоследней). }
+{ TODO -cУбить существо : Убить Н существ на локации (от первой до последней). }
+{ TODO -cПринести предмет : Собрать Н предметов с существ на локации (от первой до последней). }
+{ TODO -cПринести предмет : Найти уникальный предмет на локации (от первой до последней). }
+{ TODO -cПосетить локацию : Посетить определенную локацию (от первой до последней). }
+{ TODO -cПоговорить с НПЦ : Поговорить с определенным НПЦ (от первой до последней). }
+{ TODO -cТаймер : Доставить предмет определенному НПЦ в указанной локации за отведенный отрезок времени (от первой до последней). }
+{ TODO -cТаймер : Убить Н существ в указанной локации за отведенный отрезок времени (от первой до последней). }
 
 type
   TQuestBase = record
@@ -14,7 +23,10 @@ const
   QuestBase: array [TQuestEnum] of TQuestBase = ();
 
 type
-  TQuests = class(TObject);
+  TQuests = class(TObject)
+    constructor Create;
+    procedure Clear;
+  end;
 
 var
   Quests: TQuests;
@@ -22,6 +34,18 @@ var
 implementation
 
 uses SysUtils;
+
+{ TQuests }
+
+procedure TQuests.Clear;
+begin
+
+end;
+
+constructor TQuests.Create;
+begin
+
+end;
 
 initialization
 
