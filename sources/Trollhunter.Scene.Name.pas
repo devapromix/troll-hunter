896a274ebc156e981696982e6133b1587bdb9acb
Sergiy Tkach <bees@meta.ua>
2018-05-28 19:11:13 +0300

    Refactoring.

---
+++ sources/Trollhunter.Scene.Name.pas
@@ -0,0 +1,58 @@
+unit Trollhunter.Scene.Name;
+
+interface
+
+uses uScenes, uTypes;
+
+type
+  TSceneName = class(TScene)
+  public
+    procedure Render; override;
+    procedure Update(var Key: UInt); override;
+  end;
+
+implementation
+
+uses uTerminal, uUI, uPlayer, uGame, uLanguage, BearLibTerminal;
+
+{ TSceneName }
+
+procedure TSceneName.Render;
+begin
+  UI.Title(_('Choose a name'));
+
+  Terminal.Print(CX - 14, CY, _('Enter your player''s name') + ': ' +
+    Player.Name + Game.GetCursor, TK_ALIGN_LEFT);
+
+  AddKey('Enter', _('Confirm'));
+  AddKey('Esc', _('Back'), True);
+end;
+
+procedure TSceneName.Update(var Key: UInt);
+begin
+  case Key of
+    TK_BACKSPACE:
+      begin
+        if (Player.Name <> '') then
+          Player.Name := Copy(Player.Name, 1, Length(Player.Name) - 1);
+      end;
+    TK_ENTER, TK_KP_ENTER:
+      begin
+        if (Player.Name = '') then
+          Player.Name := _('PLAYER');
+        Scenes.SetScene(scBackground, scName);
+      end;
+    TK_A .. TK_Z:
+      begin
+        if (Length(Player.Name) < 10) then
+          Player.Name := Player.Name + Chr(Key - TK_A + 65);
+      end;
+    TK_ESCAPE:
+      begin
+        Player.Talents.Clear;
+        Scenes.SetScene(scTalents, scClass);
+      end;
+  end;
+end;
+
+end.
---
+++ sources/Trollhunter.Scene.Rest.pas
@@ -0,0 +1,52 @@
+unit Trollhunter.Scene.Rest;
+
+interface
+
+uses uScenes, uTypes;
+
+type
+  TSceneRest = class(TScene)
+  public
+    procedure Render; override;
+    procedure Update(var Key: UInt); override;
+  end;
+
+implementation
+
+{ TSceneRest }
+
+uses SysUtils, uUI, uTerminal, BearLibTerminal, uLanguage, uMsgLog, uPlayer;
+
+procedure TSceneRest.Render;
+begin
+  UI.Title(_('Rest duration'));
+
+  UI.FromAToZ;
+  Y := 1;
+
+  Inc(Y);
+  Terminal.Print(1, Y, UI.KeyToStr(Chr(Y + 95)) + ' ' + _('Rest for 10 turns'),
+    TK_ALIGN_LEFT);
+  Inc(Y);
+  Terminal.Print(1, Y, UI.KeyToStr(Chr(Y + 95)) + ' ' + _('Rest for 100 turns'),
+    TK_ALIGN_LEFT);
+  Inc(Y);
+  Terminal.Print(1, Y, UI.KeyToStr(Chr(Y + 95)) + ' ' +
+    _('Rest for 1000 turns'), TK_ALIGN_LEFT);
+
+  MsgLog.Render(2, True);
+
+  AddKey('Esc', _('Back'), True);
+end;
+
+procedure TSceneRest.Update(var Key: UInt);
+begin
+  case Key of
+    TK_A, TK_B, TK_C:
+      Player.Rest(StrToInt('1' + StringOfChar('0', Key - TK_A + 1)));
+    TK_ESCAPE:
+      Scenes.SetScene(scGame);
+  end
+end;
+
+end.
