ea554a0288a768dbf494da7268c971ee07a15c5d
Sergiy Tkach <bees@meta.ua>
2018-06-24 15:05:23 +0300

    Refactoring.

--- sources/uUtils.pas
+++ sources/uUtils.pas
@@ -2,14 +2,10 @@ unit uUtils;
 
 interface
 
-uses
-  Forms;
-
 type
   Utils = class(TObject)
   public
     class function GetPath(SubDir: string): string;
-    class function ShowForm(const Form: TForm; Show: Boolean = True): Integer;
   end;
 
 implementation
@@ -20,14 +16,6 @@ class function Utils.GetPath(SubDir: string): string;
 begin
   Result := ExtractFilePath(ParamStr(0));
   Result := IncludeTrailingPathDelimiter(Result + SubDir);
-end;
-
-class function Utils.ShowForm(const Form: TForm; Show: Boolean = True): Integer;
-begin
-  Form.BorderStyle := bsDialog;
-  Form.Position := poOwnerFormCenter;
-  if Show then
-    Result := Form.ShowModal;
 end;
 
 end.
