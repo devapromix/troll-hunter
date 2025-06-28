d10849c61192d50712a2d65b623290d7c0dbc8a6
devapromix <maxwof@ukr.net>
2025-06-28 12:53:01 +0300

    Refactor background scene.

--- sources/Trollhunter.Scene.Background.pas
+++ sources/Trollhunter.Scene.Background.pas
@@ -2,21 +2,28 @@ unit Trollhunter.Scene.Background;
 
 interface
 
-uses Trollhunter.Types,
+uses 
+  Trollhunter.Types,
   uScenes;
 
 type
   TSceneBackground = class(TScene)
+  private
+	procedure StartGame;
   public
     procedure Render; override;
-    procedure Update(var Key: UInt); override;
+    procedure Update(var AKey: UInt); override;
   end;
 
 implementation
 
+const
+  DELAY_MS = 1000;
+
 { TSceneBackground }
 
-uses Trollhunter.UI,
+uses
+  Trollhunter.UI,
   BearLibTerminal,
   Trollhunter.Terminal,
   Trollhunter.Player,
@@ -40,23 +47,25 @@ begin
   AddKey('Esc', _('Close'), _('Back'), True);
 end;
 
-procedure TSceneBackground.Update(var Key: UInt);
+procedure TSceneBackground.StartGame;
 begin
-  case Key of
+  Scenes.SetScene(scLoad);
+  Terminal.Refresh;
+  Terminal_Delay(DELAY_MS);
+  Map.Gen;
+  Mode.Game := True;
+  Player.Talents.DoTalent(TSceneTalents(Scenes.GetScene(scTalents)).Talent);
+  Player.StartEquip;
+  Player.StartSkills;
+  Scenes.SetScene(scGame);
+end;
+
+procedure TSceneBackground.Update(var AKey: UInt);
+begin
+  case AKey of
     TK_ENTER, TK_KP_ENTER:
       if not Mode.Game then
-      begin
-        Scenes.SetScene(scLoad);
-        Terminal.Refresh;
-        Terminal_Delay(1000);
-        Map.Gen;
-        Mode.Game := True;
-        Player.Talents.DoTalent
-          (TSceneTalents(Scenes.GetScene(scTalents)).Talent);
-        Player.StartEquip;
-        Player.StartSkills;
-        Scenes.SetScene(scGame);
-      end;
+		StartGame;
     TK_SPACE:
       if not Mode.Game then
         Player.GenerateBackground();
