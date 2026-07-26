ad4a6d219f80028e59d5573bea5c22d5448734c6
devapromix <maxwof@ukr.net>
2026-07-26 11:28:57 +0300

    Refactor.

--- sources/Trollhunter.Scenes.pas
+++ sources/Trollhunter.Scenes.pas
@@ -64,14 +64,6 @@ var
   Scenes: TScenes = nil;
 
 type
-  TSceneTitle = class(TScene)
-  public
-    procedure Render; override;
-    procedure Update(var Key: UInt); override;
-    procedure RenderHeroes();
-  end;
-
-type
   TSceneCalendar = class(TScene)
   public
     procedure Render; override;
@@ -198,6 +190,7 @@ uses
   Trollhunter.Scene.Drop,
   Trollhunter.Scene.Items,
   Trollhunter.Scene.RacesAndClasses,
+  Trollhunter.Scene.Title,
   Trollhunter.Scene.Difficulty,
   Trollhunter.Scene.Quest,
   Trollhunter.Item.Types,
@@ -439,61 +432,6 @@ begin
         not Player.IsDead then
         SetScene(scQuit, SceneEnum);
     end;
-  end;
-end;
-
-{ TSceneTitle }
-
-procedure TSceneTitle.Render;
-begin
-  Logo.Render(True);
-  Terminal.Print(Screen.Width - ((Screen.Width div 2) - (Logo.Width div 2) + 2),
-    14, Format('by Apromix v.%s', [Game.GetVersion]), TK_ALIGN_RIGHT);
-  // RenderHeroes;
-  if Mode.Wizard then
-  begin
-    Self.AddKey('Space', 'Create a new hero');
-    Self.AddKey('Z', Terminal.Colorize('Turn Wizard Mode Off', 'Red'), True);
-  end
-  else
-    Self.AddKey('Space', 'Create a new hero', True);
-end;
-
-type
-  TAJ = 'A' .. 'J';
-
-procedure TSceneTitle.RenderHeroes;
-const
-  L = 12;
-  T = 15;
-var
-  J: UInt;
-  V: TAJ;
-begin
-  Terminal.ForegroundColor(clWhite);
-  Terminal.Print(L + 4, T, 'Which hero shall you play?');
-
-  for V := 'A' to 'J' do
-  begin
-    J := Ord(V) - 65;
-    Terminal.Print(L, T + J + 2, UI.KeyToStr(V, J.ToString));
-  end;
-end;
-
-procedure TSceneTitle.Update(var Key: UInt);
-begin
-  case Key of
-    TK_ESCAPE:
-      Game.CanClose := True;
-    TK_A .. TK_J:
-      ;
-    TK_SPACE:
-      Scenes.SetScene(scDifficulty);
-    TK_ENTER, TK_KP_ENTER:
-      if Mode.Wizard then
-        Scenes.SetScene(scDifficulty);
-    TK_Z:
-      Mode.Wizard := False;
   end;
 end;
 
